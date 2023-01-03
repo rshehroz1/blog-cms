de8a149d0087f828bb083135474aac6eba2895fb
Shehroz <116956168+rshehroz1@users.noreply.github.com>
2023-01-03 19:11:04 +0500

    Delete redux-full directory

--- redux-full/src/middleware/stringMiddleware.js
+++
@@ -1,8 +0,0 @@
-const stringMiddleware = () => (next) => (action) => {
-  if(typeof action === "string") {
-    return next({type: action})
-  }
-  return next(action)
-}
-
-export default stringMiddleware
